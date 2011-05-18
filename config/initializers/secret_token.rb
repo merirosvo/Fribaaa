2d40f1ef41c6bdf2af5f54ebe7512c0eab301912
merirosvo <pjlampil@gmail.com>
2011-05-18 09:36:39 +0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fribaaa::Application.config.secret_token = '648e33f606e608448112147a1394996970ac73472374a944b6904af9bffb749ec598fb8232c4477f575e50ef9037010bedd71affbbbb7510d0cf87d433fe9f8d'
